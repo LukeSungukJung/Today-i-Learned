49dee2aab31f22a6366fcc8655a71aa8f715857c
정성욱 <xrjseka615@gmail.com>
2020-02-26 17:00:08 +0900

    rotate 60-90 is complete

--- CV/[IP]Py_ver_ch05.py
+++ CV/[IP]Py_ver_ch05.py
@@ -13,15 +13,25 @@ gray_tester = rgb2gray(original)
 
 skimage.io.imshow(original)
 #ans =skimage.transform.rotate(original,60)
-
+#60-90
 def rotate_img(theta,img):
     ori_shape= np.array(img.shape)
     cmssc = np.zeros((2,2))
     cmssc[0] = [np.cos(np.pi*theta/180),-np.sin(np.pi*theta/180)]
     cmssc[1] = [np.sin(np.pi*theta/180),np.cos(np.pi*theta/180)]
-    ex_h = ori_shape[0] - np.floor(np.dot(cmssc,[ori_shape[0],0])[0] +1)
-    ex_h = ex_h+ np.abs(ex_h+np.dot(cmssc,[0,ori_shape[1]])[0])
-    ex_h = np.uint32(np.floor(ex_h)+1)
-    res_w =np.uint32(np.floor(np.dot(cmssc,ori_shape)[1])+1)
-    res_img =  np.zeros([ex_h+ori_shape[0],res_w])
+    ex_h_top = np.floor(np.dot(cmssc,[0,ori_shape[1]])[0] +1)
+    pad_h = np.uint32(np.abs(np.floor(np.dot(cmssc,[0,ori_shape[1]])[0])))    
+    ex_h =np.uint32(np.abs(ex_h_top))
+    ex_h_botton = np.uint32(np.abs(np.floor(np.dot(cmssc,[ori_shape[0],0])[0] +1)))
+    pd_h = (ex_h + pad_h)
+    ex_w =np.uint32(np.floor(np.dot(cmssc,ori_shape)[1])+1)
+    res_img =  np.zeros([pd_h,ex_w])
+
+    for x in range(0,ori_shape[0]):
+        for y in range(0,ori_shape[1]):
+            tmp_x_y = np.dot(cmssc,[x,y])
+            tmp_x_y[0]= np.abs(np.floor(tmp_x_y[0]+pad_h+1))
+            tmp_x_y[1]= np.abs(np.floor(tmp_x_y[1]+1))
+            res_img[np.uint32(tmp_x_y[0]),np.uint32(tmp_x_y[1])] = img[x,y]
+
     return res_img
